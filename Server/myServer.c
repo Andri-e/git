cfd7d4f842e763463b346981722f1ad39d476f6f
Andri-e <55386300+Andri-e@users.noreply.github.com>
2020-10-28 12:40:17 +0100

    Update myServer.c

--- Server/myServer.c
+++ Server/myServer.c
@@ -33,6 +33,29 @@ static void beforeReadVariable(UA_Server *server,
 static void checkArguments(UA_Server *server)
 {
 	printf("Some text AGGG \n AGGG \n ahhh!");
+	
+	//Check for Arguments, host name and port number
+    if(argc > 2)
+    {
+		UA_Int16 port_number = atoi(argv[2]);
+		UA_ServerConfig_setMinimal(UA_Server_getConfig(server), port_number, 0);
+    } 
+    else
+    {	
+		UA_ServerConfig_setDefault(UA_Server_getConfig(server));
+    }
+
+    if(argc > 1)
+    {
+		//Copy the hostname from char * to an open62541 Variable
+		UA_String hostname;
+		UA_String_init(&hostname);
+		hostname.length = strlen(argv[1]);
+		hostname.data = (UA_Byte *) argv[1];
+
+		//Change the configuration 
+		UA_ServerConfig_setCustomHostname(UA_Server_getConfig(server), hostname);
+    }
 }
 
 
@@ -44,7 +67,7 @@ int main(int argc, char * argv[])
     signal(SIGTERM, stopHandler);
 
     UA_Server *server = UA_Server_new();
-
+/*
     //Check for Arguments, host name and port number
     if(argc > 2)
     {
@@ -67,7 +90,7 @@ int main(int argc, char * argv[])
 		//Change the configuration 
 		UA_ServerConfig_setCustomHostname(UA_Server_getConfig(server), hostname);
     }
-	
+*/	
 	checkArguments(server);
 
     //Add a new namespace to the server
