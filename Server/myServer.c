24aae0ba5f6a70928d150a70c2c9022cecec7e57
Andri-e <55386300+Andri-e@users.noreply.github.com>
2020-11-03 12:53:39 +0100

    Update myServer.c

--- Server/myServer.c
+++ Server/myServer.c
@@ -223,8 +223,10 @@ static void beforeReadIdle(UA_Server *server,
 			}
 		}
 		idleFraction = 100 - (idle-lastIdle)*100.0/(sum-lastSum);
-		printf("Busy for : %lf %% of the time.", idleFraction);
+		printf("\n\nBusy for : %lf %% of the time.", idleFraction);
 		sysidle = (double)idleFraction;
+		
+		printf("\nBusy for : %f %% of the time.\n\n", idleFraction);
 		
 		
 		lastIdle = idle;
