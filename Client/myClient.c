06722ac9da2d1e8ab457e7d37cfb354a84e41642
Andri-e <55386300+Andri-e@users.noreply.github.com>
2020-11-03 14:14:35 +0100

    Update myClient.c

--- Client/myClient.c
+++ Client/myClient.c
@@ -4,8 +4,6 @@
 #include <stdlib.h>
 
 UA_Boolean running = true;
-UA_DateTime timeStamp = 0;
-UA_DateTimeStruct dts = UA_DateTime_toStruct(timeStamp);	
 
 // ---------------------------------------
 
@@ -27,7 +25,7 @@ static void readNode(UA_Client *client, UA_StatusCode retval, UA_Variant value)
     //Variables for read access 
     UA_String variableName;
     UA_Int32 serialNumber;
-	//UA_DateTime timeStamp;
+	UA_DateTime timeStamp;
 	UA_Float sysTemp;
 	UA_Double sysIdle;
 	
