5bb55f766192d89dc553c5bad33f583e207995b3
Andri-e <55386300+Andri-e@users.noreply.github.com>
2020-11-03 13:17:13 +0100

    Update myLoad.c

--- Load/myLoad.c
+++ Load/myLoad.c
@@ -9,10 +9,23 @@
 #include<unistd.h>
 #include<stdio.h>
 
+static volatile bool running = true;
+
+// Stop handler to watch for ctrl + c 
+static void stopHandler(int sig)
+{
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+    running = false;
+}
+
+
 // myServer main 
 int main(int argc, char * argv[])
 {
-	while (1)
+	signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+	while (running == true)
 	{
 		long int i = 1; 
 		for (int a = 0; a < 10000; ++a)
