cdc80c86642188cd6f772953e0a84ba176774576
Andri-e <55386300+Andri-e@users.noreply.github.com>
2020-11-03 14:27:16 +0100

    Update myClient.c

--- Client/myClient.c
+++ Client/myClient.c
@@ -79,18 +79,11 @@ static void readNode(UA_Client *client, UA_StatusCode retval, UA_Variant value)
 	
 	
 	
-   long            ms; // Milliseconds
-    time_t          s;  // Seconds
-    struct timespec spec;
+struct timeval  tv;
+gettimeofday(&tv, NULL);
 
-    clock_gettime(CLOCK_REALTIME, &spec);
-
-    s  = spec.tv_sec;
-    ms = (spec.tv_nsec / 1.0e6); // Convert nanoseconds to milliseconds
-    if (ms > 999) {
-        s++;
-        ms = 0;
-    }
+double time_in_mill = 
+         (tv.tv_sec) * 1000 + (tv.tv_usec) / 1000 ;
 
     printf("Current time: %"PRIdMAX".%03ld seconds since the Epoch\n",
            (intmax_t)s, ms);
