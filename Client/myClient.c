8abf5771afa31000faa6f8570b86d60434c05e1e
andri-e <55386300+Andri-e@users.noreply.github.com>
2020-11-12 20:12:04 +0100

    asd

--- Client/myClient.c
+++ Client/myClient.c
@@ -29,14 +29,14 @@ static void readNode(UA_Client *client, UA_StatusCode retval, UA_Variant value)
 	UA_DateTimeStruct dts = UA_DateTime_toStruct(timeStamp);	
 
 
-/*
+
     //Read Variable name
     retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(2, "testVariableName"), &value);
     if(retval == UA_STATUSCODE_GOOD && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_STRING])) 
     {
 		variableName = *(UA_String *) value.data;
     }
-
+/*
     //Read Serial Number 
     retval = UA_Client_readValueAttribute(client, UA_NODEID_STRING(2, "testSerial"), &value);
     if(retval == UA_STATUSCODE_GOOD && UA_Variant_hasScalarType(&value, &UA_TYPES[UA_TYPES_INT32])) 
