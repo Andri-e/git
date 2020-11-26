960950dee4bd24203e4e1060f3add2334e5f5a34
andri-e <55386300+Andri-e@users.noreply.github.com>
2020-11-26 17:08:43 +0100

    asd

--- Server/myServer.c
+++ Server/myServer.c
@@ -123,7 +123,7 @@ static void
 addIncInt32ArrayMethod(UA_Server *server) 
 {
     /* Two input arguments */
-    UA_Argument inputArguments[1];
+    UA_Argument inputArguments[3];
     UA_Argument_init(&inputArguments[0]);
     inputArguments[0].description = UA_LOCALIZEDTEXT("en-US", "int32[5] array");
     inputArguments[0].name = UA_STRING("int32 array");
