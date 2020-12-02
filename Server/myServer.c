51de4575b6348018e1a39f204910592031eb809d
andri-e <55386300+Andri-e@users.noreply.github.com>
2020-12-02 17:26:38 +0100

    Makefile fix for method

--- Server/myServer.c
+++ Server/myServer.c
@@ -12,6 +12,8 @@
 #include "./Include/myNodes.h"
 // specNodes.h
 #include "./Include/specNodes.h"
+// Methods 
+#include "./Includes/myMethod.h"
 
 // Want to add 
         // Methods 
@@ -24,85 +26,6 @@
         // Conditions? 
             //
         
-
-
-UA_StatusCode MethodCallback(UA_Server *server,
-                         const UA_NodeId *sessionId, void *sessionHandle,
-                         const UA_NodeId *methodId, void *methodContext,
-                         const UA_NodeId *objectId, void *objectContext,
-                         size_t inputSize, const UA_Variant *input,
-                         size_t outputSize, UA_Variant *output) 
-{
-    UA_String *inputStr = (UA_String*)input->data;
-    UA_String response = UA_STRING_ALLOC("Undefined");
-
-    if(inputStr->length == 1)
-    {
-        switch(inputStr->data[0])
-        {
-            case '1' :
-                printf("Case 1 \n");
-                response = UA_STRING_ALLOC("Case 1 selected.");
-                break;
-            case '2' :
-                printf("Case 2 \n");
-                response = UA_STRING_ALLOC("Case 2 selected.");
-                break;
-            case '3' :
-                printf("Case 3 \n");
-                response = UA_STRING_ALLOC("Case 3 selected.");
-                break;
-            default : 
-                printf("Incorect value picked. \n");
-                response = UA_STRING_ALLOC("Incorect value picked.");
-                break;
-        }
-    } 
-    else 
-    {
-        printf("String to long, please try again.\n");
-        response = UA_STRING_ALLOC("String to long, please try again.");
-    }
-
-
-    response.data = (UA_Byte *)UA_realloc(response.data, response.length);
-    UA_Variant_setScalarCopy(output, &response, &UA_TYPES[UA_TYPES_STRING]);
-    
-    // Cleanup 
-    UA_String_clear(&response);
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, "Method was called");
-    return UA_STATUSCODE_GOOD;
-}
-
-void addMethodNode(UA_Server *server) 
-{
-    UA_Argument inputArgument;
-    UA_Argument_init(&inputArgument);
-    inputArgument.description = UA_LOCALIZEDTEXT("en-US", "A String");
-    inputArgument.name = UA_STRING("MyInput");
-    inputArgument.dataType = UA_TYPES[UA_TYPES_STRING].typeId;
-    inputArgument.valueRank = UA_VALUERANK_SCALAR;
-
-    UA_Argument outputArgument;
-    UA_Argument_init(&outputArgument);
-    outputArgument.description = UA_LOCALIZEDTEXT("en-US", "A String");
-    outputArgument.name = UA_STRING("MyOutput");
-    outputArgument.dataType = UA_TYPES[UA_TYPES_STRING].typeId;
-    outputArgument.valueRank = UA_VALUERANK_SCALAR;
-
-    UA_MethodAttributes helloAttr = UA_MethodAttributes_default;
-    helloAttr.description = UA_LOCALIZEDTEXT("en-US","Select : \t1 for xx \n\t2 for xy\n\t3 for yx\n\t4 for ... ");
-    helloAttr.displayName = UA_LOCALIZEDTEXT("en-US","Method Node");
-    helloAttr.executable = true;
-    helloAttr.userExecutable = true;
-    UA_Server_addMethodNode(server, UA_NODEID_NUMERIC(1,62541),
-                            UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
-                            UA_NODEID_NUMERIC(0, UA_NS0ID_HASCOMPONENT),
-                            UA_QUALIFIEDNAME(1, "MethodNode"),
-                            helloAttr, &MethodCallback,
-                            1, &inputArgument, 1, &outputArgument, NULL, NULL);
-}
-
 static volatile UA_Boolean running = true;			// Server state
 
 // Stop handler to watch for ctrl + c 
