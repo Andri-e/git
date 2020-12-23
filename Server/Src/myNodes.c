dcab23242b72328ceca1a81bc0a931db67bb1ac1
andri-e <55386300+Andri-e@users.noreply.github.com>
2020-12-23 17:40:10 +0100

    asd

--- Server/Src/myNodes.c
+++ Server/Src/myNodes.c
@@ -20,7 +20,7 @@ void nodeSetup(UA_Server *server)
     oAttr.displayName = UA_LOCALIZEDTEXT("en-US", "Device name");
     oAttr.description = UA_LOCALIZEDTEXT("en-US", "This is a test device");
     UA_Server_addObjectNode(server, UA_NODEID_STRING(2, "testObject"),
-                            UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                            UA_NODEID_NUMERIC(0, UA_NS0ID_ROOTFOLDER ),
                             UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES),
                             UA_QUALIFIEDNAME(2, "Test Object"), 
                             UA_NODEID_NUMERIC(0, UA_NS0ID_BASEOBJECTTYPE),
