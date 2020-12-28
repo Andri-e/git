f1d945ad0b65c1929387b5097f3eaa6ba69daf15
andri-e <55386300+Andri-e@users.noreply.github.com>
2020-12-28 14:24:33 +0100

    asd

--- Server/Src/myEvent.c
+++ Server/Src/myEvent.c
@@ -5,14 +5,14 @@ static UA_NodeId eventType;
 
 UA_StatusCode addNewEventType(UA_Server *server) 
 {
-    UA_ObjectTypeAttributes attrEvent = UA_ObjectTypeAttributes_default;
-    attrEvent.displayName = UA_LOCALIZEDTEXT("en-US", "SimpleEventType");
-    attrEvent.description = UA_LOCALIZEDTEXT("en-US", "A simple event to turn on");
+    UA_ObjectTypeAttributes attrOn = UA_ObjectTypeAttributes_default;
+    attrOn.displayName = UA_LOCALIZEDTEXT("en-US", "SimpleEventType");
+    attrOn.description = UA_LOCALIZEDTEXT("en-US", "A simple event to turn on");
     return UA_Server_addObjectTypeNode(server, UA_NODEID_NULL ,
                                        UA_NODEID_NUMERIC(0, UA_NS0ID_BASEEVENTTYPE),
                                        UA_NODEID_NUMERIC(0, UA_NS0ID_HASSUBTYPE),
-                                       UA_QUALIFIEDNAME(0, "EventType"),
-                                       attrEvent, 
+                                       UA_QUALIFIEDNAME(0, "SimpleEventType"),
+                                       attrOn, 
                                        NULL, &eventType);
 }
 
