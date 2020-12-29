9b391adcd0137047833dd87f51e9345e1752e9a5
andri-e <55386300+Andri-e@users.noreply.github.com>
2020-12-29 18:30:58 +0100

    asd

--- Temp/Raspi_2/Client2/myClient2.c
+++ Temp/Raspi_2/Client2/myClient2.c
@@ -86,10 +86,15 @@ int main(void)
     // default timeout is 5 seconds. Set it to 1 second here for demo 
    // cc->timeout = 1000;
 
+    UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://192.168.1.59:4840");
+    if(retval != UA_STATUSCODE_GOOD) {
+        UA_Client_delete(client);
+        return (int)retval;
+    }
 
  //   UA_Variant value; /* Variants can hold scalar values and arrays of any type */
  //   UA_Variant_init(&value);
-	UA_StatusCode retval = UA_Client_connect(client, SERVER_ENDPOINT);
+	//UA_StatusCode retval = UA_Client_connect(client, SERVER_ENDPOINT);
 	
 //	printf("\t\t\t\t\t\t\tUser Name \tUser Id \tTime Stamp \t\tVariable\n");	
 	// While loop that keeps reading the value from the server until it is disconnected 
@@ -97,7 +102,8 @@ int main(void)
 	{
         if(retval != UA_STATUSCODE_GOOD) 						// If status code not good then log time and try to reconect 
 		{ 
-			UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_CLIENT, "Not connected. Retrying to connect in 2 second");		
+			UA_LOG_ERROR(UA_Log_Stdout, UA_LOGCATEGORY_CLIENT, "Not connected. Retrying to connect in 2 second");	
+            UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://192.168.1.59:4840");	
             UA_sleep_ms(2000);
 			UA_StatusCode retval = UA_Client_connect(client, SERVER_ENDPOINT);
             continue;
