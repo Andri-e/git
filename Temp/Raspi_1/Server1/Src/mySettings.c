d08078987c0fb8f5588c6d21ae38e676ff07191e
andri-e <55386300+Andri-e@users.noreply.github.com>
2021-01-01 23:08:44 +0100

    asd

--- Temp/Raspi_1/Server1/Src/mySettings.c
+++ Temp/Raspi_1/Server1/Src/mySettings.c
@@ -5,21 +5,10 @@
 void checkArguments(UA_Server *server, int argc, char * argv[] )
 {
 	//Check for Arguments, host name and port number
-    if(argc > 2)
+    if(argc != 0)
     {
 		UA_Int16 port_number = atoi(argv[2]);
 		UA_ServerConfig_setMinimal(UA_Server_getConfig(server), port_number, 0);
-    }
-	else if(argc > 1)
-    {
-		//Copy the hostname from char * to an open62541 Variable
-		UA_String hostname;
-		UA_String_init(&hostname);
-		hostname.length = strlen(argv[1]);
-		hostname.data = (UA_Byte *) argv[1];
-
-		//Change the configuration 
-		UA_ServerConfig_setCustomHostname(UA_Server_getConfig(server), hostname);
     }
 	else
     {	
