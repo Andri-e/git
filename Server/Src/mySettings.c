65267e052812bf7e539369c1b8e5bd5cf85e3d50
andri-e <55386300+Andri-e@users.noreply.github.com>
2021-01-03 13:40:28 +0100

    asd

--- Server/Src/mySettings.c
+++ Server/Src/mySettings.c
@@ -5,7 +5,7 @@
 void checkArguments(UA_Server *server, int argc, char * argv[] )
 {
 	//Check for Arguments, host name and port number
-    if(argc > 2)
+    if(argc > 1)
     {
 		UA_Int16 port_number = atoi(argv[2]);
 		UA_ServerConfig_setMinimal(UA_Server_getConfig(server), port_number, 0);
@@ -13,18 +13,6 @@ void checkArguments(UA_Server *server, int argc, char * argv[] )
     else
     {	
 		UA_ServerConfig_setDefault(UA_Server_getConfig(server));
-    }
-
-    if(argc > 1)
-    {
-		//Copy the hostname from char * to an open62541 Variable
-		UA_String hostname;
-		UA_String_init(&hostname);
-		hostname.length = strlen(argv[1]);
-		hostname.data = (UA_Byte *) argv[1];
-
-		//Change the configuration 
-		UA_ServerConfig_setCustomHostname(UA_Server_getConfig(server), hostname);
     }
 }
 
